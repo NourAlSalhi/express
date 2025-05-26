ac618326521dc87f3bbd2ad55d011890f1c5a51b
Nour Al Salhi <nuraym9998@gmail.com>
2025-05-26 13:04:19 +0300

    add post, put and delete method in app

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ import data from './data/mock.json' assert { type: 'json' };
 const app = express();
 const PORT = 3000;
 
+app.get("/",(req,res) => {
+  res.json(data)
+})
+
+app.post("/create",(req,res) => {
+  res.send("This is a Post Reauest at /create")
+})
+
+app.put("/edit",(req,res) => {
+  res.send("This is a Edit Reauest at /edit")
+})
+
+app.delete("/delete",(req,res) => {
+  res.send("This is a Delete Reauest at /delete")
+})
+
 app.listen(PORT, () => {
   console.log("Server is running on port 3000")
-  console.log(data)
 })
