03486a58c2f5bed5e5952f6dcfa7cf9185491c04
Nour Al Salhi <nuraym9998@gmail.com>
2025-05-31 10:17:46 +0300

    add some routes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-import express from 'express';
-import data from './data/mock.json' assert { type: 'json' };
-
+import express from "express";
+import data from "./data/mock.json" assert { type: "json" };
 
 const app = express();
 const PORT = 3000;
@@ -9,24 +8,47 @@ const PORT = 3000;
 app.use(express.static("public"));
 
 //Using the images folder at the root/images
-app.use("/images",express.static("images"));
+app.use("/images", express.static("images"));
 
-app.get("/",(req,res) => {
-  res.json(data)
+app.get("/", (req, res) => {
+  res.json(data);
+});
+
+app.get("/class/:id", (req, res) => {
+  const studentId = Number(req.params.id);
+  const student = data.filter((student) => student.id === studentId);
+  res.send(student);
+});
+
+app.get("/redirect", (req, res) => {
+  res.redirect("https://www.google.com");
+});
+
+app.route("/class").get((req, res) => {
+  res.send("This is a Get Reauest at /class");
+})
+.post((req, res) => {
+  res.send("This is a Post Reauest at /class");
+})
+.put((req, res) => {
+  res.send("This is a Put Reauest at /class");
+})
+.delete((req, res) => {
+  res.send("This is a Delete Reauest at /class");
 })
 
-app.post("/create",(req,res) => {
-  res.send("This is a Post Reauest at /create")
-})
+app.post("/create", (req, res) => {
+  res.send("This is a Post Reauest at /create");
+});
 
-app.put("/edit",(req,res) => {
-  res.send("This is a Edit Reauest at /edit")
-})
+app.put("/edit", (req, res) => {
+  res.send("This is a Edit Reauest at /edit");
+});
 
-app.delete("/delete",(req,res) => {
-  res.send("This is a Delete Reauest at /delete")
-})
+app.delete("/delete", (req, res) => {
+  res.send("This is a Delete Reauest at /delete");
+});
 
 app.listen(PORT, () => {
-  console.log("Server is running on port 3000")
-})
+  console.log("Server is running on port 3000");
+});
